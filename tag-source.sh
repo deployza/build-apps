53c4346c68b13aa3aa92934e44b39c7c24e20ad6
Admin <romil.jain@deployza.com>
2025-08-19 18:00:16 +0530

    fixig bugs

--- tag-source.sh
+++
@@ -1,6 +0,0 @@
-source /workspace/version.txt
-
-git config user.email "cloudbuild@ydeployza.com"
-git config user.name "Cloud Build"
-git tag -a "${VERSION}" -m "Release ${VERSION}"
-git push origin "${VERSION}" --force
