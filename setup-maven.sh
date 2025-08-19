ccb9c59caff6610ee7c5d994afefc2d2dde2aec8
Admin <romil.jain@deployza.com>
2025-08-19 17:16:33 +0530

    added code

---
+++ setup-maven.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -e  
+
+echo "***** configuring settings.xml"
+
+#Create .m2 directory
+mkdir -p /workspace/.m2
+
+#Create settings.xml
+
+cat > /workspace/.m2/settings.xml <<"EOF"
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                              https://maven.apache.org/xsd/settings-1.0.0.xsd">
+  <servers>
+    <server>
+      <id>snapshot-repository</id>
+      <username>oauth2accesstoken</username>
+      <password>__ACCESS_TOKEN__</password>
+    </server>
+    <server>
+      <id>release-repository</id>
+      <username>oauth2accesstoken</username>
+      <password>__ACCESS_TOKEN__</password>
+    </server>    
+    <server>
+      <id>maven-central-repository</id>
+      <username>oauth2accesstoken</username>
+      <password>__ACCESS_TOKEN__</password>
+    </server>    
+    <server>
+      <id>maven-central-plugin-repository</id>
+      <username>oauth2accesstoken</username>
+      <password>__ACCESS_TOKEN__</password>
+    </server>    
+  </servers>
+</settings>
+
+EOF
+
+#get ACCESS Token
+ACCESS_TOKEN=$(gcloud auth print-access-token)
+
+#Replace in the settings.xml
+sed -i "s|__ACCESS_TOKEN__|${ACCESS_TOKEN}|" /workspace/.m2/settings.xml
+
+echo "***** configuring version.txt"
+
+#Get build version and put in a file
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+echo "VERSION=$VERSION" >> /workspace/version.txt
+
+
+echo "***** configuring .git-credentials"
+# Use gcloud to retrieve the token (same as Solution 1) --project=$PROJECT_ID
+BITBUCKET_TOKEN="$(gcloud secrets versions access latest --secret=bitbucket-admin-api-token)"
+
+# Configure the credential helper to use a memory cache
+ # The syntax is https://<username>:<token>@<host>
+git config --global credential.helper 'store --file=/workspace/.git-credentials'
+#git config --global url."https://x-token-auth:${BITBUCKET_TOKEN}@bitbucket.org".insteadOf "https://bitbucket.org"
+
+# Add the credentials to the file
+echo "https://x-token-auth:${BITBUCKET_TOKEN}@bitbucket.org" > /workspace/.git-credentials
+
+#echo "***** settings.xml..."
+#cat /workspace/.m2/settings.xml
+#echo "***** version.txt..."
+#cat /workspace/version.txt
+#echo "***** .git-credentials..."
+#cat /workspace/.git-credentials
