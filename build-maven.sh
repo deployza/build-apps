65bba33bf9ae495bacb939ab8a96d157181218f7
Admin <romil.jain@deployza.com>
2025-12-06 16:14:26 +0530

    dev

--- build-maven.sh
+++ build-maven.sh
@@ -1,35 +1,26 @@
 #!/bin/bash
 
+#exit on any error
 set -e  
 
 # CONFIGURE MAVEN SETTINGS.XML
 echo "***** configuring settings.xml"
-
-#Create .m2 directory
 mkdir -p /workspace/.m2
-
-#Create settings.xml
-
 cp ./build-scripts/maven-settings.xml /workspace/.m2/settings.xml
 
 #get ACCESS Token
 ACCESS_TOKEN=$(gcloud auth print-access-token)
-
 #Replace in the settings.xml
 sed -i "s|__ACCESS_TOKEN__|${ACCESS_TOKEN}|" /workspace/.m2/settings.xml
 
 # CONFIGURE VERSION.TXT
-
-echo "***** configuring version.txt"
-
 #Get build version and put in a file
-mvn help:evaluate -Dexpression=project.version -q -DforceStdout -s /workspace/.m2/settings.xml
+echo "***** configuring version.txt"
 VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout -s /workspace/.m2/settings.xml)
 
 echo "VERSION=$VERSION" >> /workspace/version.txt
 
 # CONFIGURE BITBUCKET CREDENTIALS
-
 echo "***** configuring .git-credentials"
 # Use gcloud to retrieve the token (same as Solution 1) --project=$PROJECT_ID
 BITBUCKET_TOKEN="$(gcloud secrets versions access latest --secret=bitbucket-admin-api-token)"
@@ -48,9 +39,8 @@ echo "***** runing maven"
 mvn -B -s /workspace/.m2/settings.xml -Dmaven.repo.local=/workspace/.m2 clean deploy
 
 #TAG SOURCE CODE
-
 echo "***** tagging source code"
-#source /workspace/version.txt
+source /workspace/version.txt
 git config user.email "cloudbuild@ydeployza.com"
 git config user.name "Cloud Build"
 git tag -a "${VERSION}" -m "Release ${VERSION}"
